13d300f450f20fe19b9e6ea28e3db3bc968d700a
Carl Brumm <26975605+c-e-brumm@users.noreply.github.com>
2019-08-15 16:25:37 -0700

    A bare bones operator that will set your ClusterDeployments to "Installed: true". Does nothing else.

---
+++ pkg/controller/clusterdeployment/clusterdeployment_controller.go
@@ -0,0 +1,99 @@
+package clusterdeployment
+
+import (
+	"context"
+
+	hivev1alpha1 "github.com/openshift/hive/pkg/apis/hive/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller_clusterdeployment")
+
+const (
+	controllerName                = "clusterdeployment"
+	ClusterDeploymentManagedLabel = "api.openshift.com/managed"
+)
+
+// Add creates a new ClusterDeployment Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileClusterDeployment{
+		client: mgr.GetClient(),
+		scheme: mgr.GetScheme(),
+	}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New(controllerName+"-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource ClusterDeployment
+	err = c.Watch(&source.Kind{Type: &hivev1alpha1.ClusterDeployment{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileClusterDeployment{}
+
+// ReconcileClusterDeployment reconciles a ClusterDeployment object
+type ReconcileClusterDeployment struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a ClusterDeployment object and makes changes based on the state read
+// and what is in the ClusterDeployment.Spec
+// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
+// a Pod as an example
+// Note:
+// The Controller will requeue the Request to be processed again if the returned error is non-nil or
+// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+func (r *ReconcileClusterDeployment) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling ClusterDeployment")
+
+	// Fetch the ClusterDeployment instance
+	cd := &hivev1alpha1.ClusterDeployment{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, cd)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+	cd.Status.Installed = true
+	err = r.client.Status().Update(context.TODO(), cd)
+	if err != nil {
+		reqLogger.Error(err, err.Error())
+		return reconcile.Result{}, err
+
+	}
+	reqLogger.Info("updating status")
+	return reconcile.Result{}, nil
+}
